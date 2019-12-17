1b4e6d703807dc0208ef1a5cc53453a003dbc8bd
alsterium <new033@gmail.com>
2019-12-17 14:07:28 +0900

    add camtest

---
+++ html/script/camtest.js
@@ -0,0 +1,34 @@
+import { Stream } from "stream";
+
+window.onload = () => {
+    const video = document.querySelector("#camera");
+    const canvas = document.querySelector("#picture");
+
+    /*camera settings*/
+
+    const constraints = {
+        audio: false,
+        video: {
+            width: 300,
+            height: 200,
+            facingMode: "user"
+        }
+    };
+
+    /*sync camera to <video> tag */
+    navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+            video.srcObject = stream;
+            video.onloadedmetadata = (e) => {
+                video.play();
+            };
+        })
+        .catch((err) => {
+            console.log(err.name + ":" + err.message);
+        });
+
+    /*シャッターボタン */
+    document.querySelector("#shutter").addEventListener("click", () => {
+        const ctx = canvas.getContext("2d");
+        ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+    });
+}
