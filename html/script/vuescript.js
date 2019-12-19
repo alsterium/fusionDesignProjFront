fba140f7a3de8bb7589974fba0fb9c90c9f98596
alsterium <new033@gmail.com>
2019-12-19 16:06:12 +0900

    add activate camera

--- html/script/vuescript.js
+++ html/script/vuescript.js
@@ -1,10 +1,40 @@
 new Vue({
-  el: "#app",
-  methods: {
-    grabImage: function() {
-      let canvas = document.getElementById("model_preview");
-      let link = document.getElementById("dllink");
-      link.href = canvas.toDataURL();
+    el: "#app",
+    data: {
+        toggleView: true
+    },
+    methods: {
+        grabImage: function() {
+            let canvas = document.getElementById("model_preview");
+            let link = document.getElementById("dllink");
+            link.href = canvas.toDataURL();
+        },
+        activateCamera: function() {
+            this.toggleView = !this.toggleView;
+            console.log(this.toggleView);
+            if (this.toggleView != true) {
+                const video = document.getElementById("cam_preview");
+
+                /*camera settings*/
+
+                const constraints = {
+                    audio: false,
+                    video: {
+                        facingMode: "environment"
+                    }
+                };
+
+                /*sync camera to <video> tag */
+                navigator.mediaDevices
+                    .getUserMedia(constraints)
+                    .then(stream => {
+                        video.srcObject = stream;
+                    })
+                    .catch(err => {
+                        console.log(err.name + ":" + err.message);
+                    });
+            }
+
+        }
     }
-  }
 });
