778a7ed0aede308f6cd06fdb7f3b12b62b76929e
alsterium <new033@gmail.com>
2020-02-03 18:31:59 +0900

    リファクタリング

--- html/script/stampScript.js
+++ html/script/stampScript.js
@@ -3,33 +3,44 @@ var canvas , stampText;
 function initStampCanvas(){
   //Canvasの初期化
   canvas = new fabric.Canvas("stampCanvas_upper");
-  stampText = "おいっす";
-  updateText();
+  var fontStyle = {
+    font:"NicoMoji",
+    size:100,
+    originX:"center",
+    originY:"center"
+  }
+  var textGroup = create_bag_character(fontStyle);
+  canvas.add(textGroup);
+
 }
 //テキストの更新
 function updateText(){
+
+};
+
+function create_bag_character(font_style){
   //袋文字を作るためのテキスト
-  var upperText = new fabric.Text(stampText, {
-    fontFamily: "Nico Moji",
-    fontSize: 100,
-    originX: "center",
-    originY: "center"
+  var upperText = new fabric.Text(font_style.text, {
+    fontFamily: font_style.font,
+    fontSize: font_style.size,
+    originX: font_style.originX,
+    originY: font_style.originY
   });
-  var middleText = new fabric.Text(stampText, {
-    fontFamily: "Nico Moji",
-    fontSize: 100,
+  var middleText = new fabric.Text(font_style.text, {
+    fontFamily: font_style.font,
+    fontSize: font_style.size,
     stroke: "#ffffff",
     strokeWidth: 10,
-    originX: "center",
-    originY: "center"
+    originX: font_style.originX,
+    originY: font_style.originY
   });
-  var lowerText = new fabric.Text(stampText, {
-    fontFamily: "Nico Moji",
-    fontSize: 100,
+  var lowerText = new fabric.Text(font_style.text, {
+    fontFamily: font_style.font,
+    fontSize: font_style.size,
     stroke: "#000",
     strokeWidth: 15,
-    originX: "center",
-    originY: "center"
+    originX: font_style.originX,
+    originY: font_style.originY
   });
 
   //二重袋文字
@@ -37,7 +48,5 @@ function updateText(){
     top: 150,
     left: 150
   });
-  //文字をCanvasに追加
-  canvas.add(textGroup);
-};
-
+  return textGroup;
+}
